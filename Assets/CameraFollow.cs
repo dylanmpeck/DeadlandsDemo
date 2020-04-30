508a89467649c6624d4b541e98bb1213cfe311dc
Dylan Peck <dylanmpeck@gmail.com>
2020-04-30 12:22:08 -0700

    Creating repo: Imported western premades and set up top down player locomotion

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+    [SerializeField] float keepAwayDistance;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 followPos = target.transform.position + Vector3.back * keepAwayDistance;
+        transform.position = new Vector3(followPos.x, transform.position.y, followPos.z);
+        transform.LookAt(target.transform, Vector3.up);
+        transform.RotateAround(target.transform.position, Vector3.right, 55.0f - transform.eulerAngles.x);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+    Animator playerAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0.0f || Input.GetAxisRaw("Vertical") != 0.0f)
+        {
+            playerAnimator.SetBool("Running", true);
+            //playerAnimator.SetFloat("Runspeed", Input.GetAxisRaw("Vertical"));
+            Vector3 inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+            if (inputDirection.magnitude > 1.0f)
+            {
+                inputDirection = inputDirection.normalized;
+            }
+
+            Vector3 moveDirection = transform.InverseTransformDirection(inputDirection);
+            playerAnimator.SetFloat("XAxis", moveDirection.x);
+            playerAnimator.SetFloat("YAxis", moveDirection.z);
+            transform.Translate(moveDirection * Time.deltaTime * 4.0f);
+        }
+        else
+        {
+            playerAnimator.SetBool("Running", false);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            playerAnimator.SetTrigger("Shoot");
+        }
+
+        LookAtMousePos();
+    }
+
+    void LookAtMousePos()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * Vector3.Distance(transform.position, Camera.main.transform.position));
+        mousePos.y = transform.position.y;
+        Vector3 lookDir = (mousePos - transform.position).normalized;
+        transform.LookAt(mousePos);
+    }
+}
